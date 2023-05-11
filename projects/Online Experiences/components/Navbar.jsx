0e32a01ef34587a24a9e40ebca48bd6bac0abbdd
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-05-11 16:26:11 +0800

    remove all

--- projects/Online Experiences/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Navbar(){
-    return (
-        <nav className="navbar">
-            <img className="navlogo" src="../img/airbnb-logo.png" alt="img"/>
-        </nav>
-    )
-}
