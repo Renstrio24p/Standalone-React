be920106bb8e1b540cf363d73352a9c7555f089c
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-05-11 12:58:25 +0800

    add files

---
+++ index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ReactApp from "./react-folder/react";
+import Start from "./start";
+
+const DOM = ReactDOM.createRoot(document.getElementById('root'))
+DOM.render(<ReactApp />)
---
+++ start.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Start(){
+    return (
+    <div>
+        <h1> Hello React! </h1>
+        {/* You can now code right here */}
+    </div>
+    )
+}
