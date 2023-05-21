e5fb4408f89a8c2103e432e760238c17e50f9772
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-05-21 17:24:56 +0800

    add all

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports={
     entry: "./src/index.jsx", 
     output: {
 
-        path: path.resolve(__dirname, "./runtime"),
+        path: path.resolve(__dirname, "./dist"),
         
         filename: "index.react.js"
     },
