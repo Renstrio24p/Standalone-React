843ff6207a6b768f3227aacd8194f488d2a65e4f
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-06-15 17:42:16 +0800

    add styles

--- src/start.jsx
+++ src/start.jsx
@@ -4,13 +4,12 @@ import { Join } from "./assets/util/JoinClasses";
 
 export default function Start(){
     return (
-        <div>
-            <h1 className={Join([styles.weight,styles.pad])}>
-                 Hello React 
-                <span className={Join([styles.fonts,styles.weight])}>
-                    18
-                </span>
-            </h1>
+        <div className={styles['d-flex-center']}>
+            <h1 className={Join([styles['dark-blue'],styles.weight,styles.pad,styles.gap20])}>
+                Hello <span className={Join([styles.weight,styles.black])}>React</span>
+            </h1> 
+            <h2 className={Join([styles['dark-cyan'],styles.weight,styles['font-size-2_5rem']])}>18</h2>
+                <img className={Join([styles['img-size-50px'],styles.react])} src="/src/assets/react.png" alt="react-logo"/>
             {/* You can now start coding right here..*/}
         </div>
     )
