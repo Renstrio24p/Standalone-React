8837544eebdc089100db2a0e2d059107d20388f0
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-09-03 04:57:56 +0800

    add files

--- webpack.config.js
+++ webpack.config.js
@@ -9,14 +9,14 @@ module.exports = {
   mode: 'development',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'webpack.bundle.js',
+    filename: 'assets/[name].[contenthash].js', // Use contenthash for cache-busting
     clean: true,
   },
   target: 'web',
   devServer: {
     port: '4500',
     static: {
-      directory: path.join(__dirname, 'src/images'),
+      directory: path.join(__dirname, 'src'),
     },
     open: true,
     hot: true,
@@ -34,6 +34,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-syntax-dynamic-import'],
           },
         },
       },
@@ -47,15 +48,43 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg|webp)$/i,
-        type: 'asset/inline',
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[contenthash][ext]', // Output path for images
+        },
+      },
+      {
+        test: /\.(mp4|webm|ogg|ogv)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'videos/[name].[contenthash][ext]', // Output path for videos
+        },
       },
     ],
   },
   optimization: {
+    minimize: true,
     minimizer: [
       new CssMinimizerPlugin(), // Minimize CSS
       new TerserPlugin(), // Minimize JavaScript
     ],
+    splitChunks: {
+      chunks: 'async',
+      minSize: 2000, // Adjust this value as needed, e.g., 20000 bytes
+      maxSize: 20000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -63,7 +92,20 @@ module.exports = {
     }),
     new CopyPlugin({
       patterns: [
-        { from: 'src/images', to: './' },
+        {
+          from: 'src/images',
+          to: 'images',
+          globOptions: {
+            ignore: ['**/placeholder.png'], // Ignore specific files if needed
+          },
+        },
+        {
+          from: 'src/videos',
+          to: 'videos',
+          globOptions: {
+            ignore: ['**/*.tmp'], // Ignore specific files if needed
+          },
+        },
       ],
     }),
   ],
