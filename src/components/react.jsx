3e3d4bddced8c85a8d3db26e40bc71a36e6875e5
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-09-03 01:04:14 +0800

    fix webpack

--- src/components/react.jsx
+++ src/components/react.jsx
@@ -6,21 +6,21 @@ export default function ReactApp() {
         <div className={styles["react-content"]}>
             <div className={styles["react-body"]}>
                 <div className={styles["react-head"]}>
-                    <a href="https://react.dev/"><img src="images/react.svg" className={styles.react}></img></a>
+                    <a href="https://react.dev/"><img src="react.svg" className={styles.react}></img></a>
                     <div className={styles["react-text"]}>
                          <h1 className={styles["react-header"]}>Standalone React</h1>
                          <p className={styles['react-pgraph']}> 
                            <span className={styles["react-title"]}>Make your life simple and organize with </span> 
-                             <img src="images/react.svg" className={styles["react-img"]}></img>
-                             <span className={styles['react-js']}> React JS.</span> 
+                             <img src="react.svg" className={styles["react-img"]}></img>
+                             <span className={styles['react-js']}> React Js.</span> 
                          </p>
                          <p className={styles["react-title"]}>Webpack Version. supports Codespace</p>
                     </div>
-                    <a href="https://webpack.js.org/"><img src="images/webpack.png" className={styles.webpack}></img></a>
+                    <a href="https://webpack.js.org/"><img src="webpack.png" className={styles.webpack}></img></a>
                 </div>
            <div className={styles.footer}>
              <p className={styles.version}>Version 1.0.6</p>
-             <a href="https://github.com/features/codespaces/" className={styles.git}> <img src="images/github.png" className={styles.github} /> Codespaces </a>
+             <a href="https://github.com/features/codespaces/" className={styles.git}> <img src="github.png" className={styles.github} /> Codespaces </a>
            </div>
            </div>
         </div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserPlugin = require('terser-webpack-plugin');
-const CompressionPlugin = require('compression-webpack-plugin'); // Import the compression plugin
 const path = require('path');
 
 module.exports = {
@@ -10,33 +7,21 @@ module.exports = {
   mode: 'development',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'webpack.bundle.[contenthash].js',
+    filename: 'webpack.bundle.js',
     clean: true,
   },
   target: 'web',
   devServer: {
-    port: '5500',
+    port: '4500',
     static: {
-      directory: path.join(__dirname, 'src/images'),
-      watch: true,
-    },
+      directory: path.join(__dirname, 'src')
+},
     open: true,
     hot: true,
     liveReload: true,
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.json'],
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
-  optimization: {
-    minimize: true, 
-    minimizer: [new TerserPlugin()],
+    extensions: ['.js', '.jsx','.json'],
   },
   module: {
     rules: [
@@ -46,57 +31,36 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-react',
-            ],
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
         },
       },
       {
-        test: /\.module\.(sa|sc|c)ss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: { modules: true },
-          },
-          'sass-loader',
-        ],
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(sa|sc|c)ss$/,
-        exclude: /\.module\.(sa|sc|c)ss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader","css-loader","sass-loader",],
       },
       {
         test: /\.(png|jpe?g|gif|svg|webp)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'images/[name].[hash][ext]',
-        },
+        type: 'asset/inline',
       },
       {
         test: /\.(mp4|webm|ogg|ogv)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'videos/[name].[hash][ext]',
-        },
-      },
+        type: 'asset/inline',
+      }
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, './', 'index.html'),
+      template: path.join(__dirname, './', 'index.html')
     }),
     new CopyPlugin({
-      patterns: [
-        { from: "src/assets/images", to: "images" },
-        // { from: "src/assets/videos", to: "videos" },
-      ],
-    }),    
-    new MiniCssExtractPlugin({ filename: 'styles.[contenthash].css' }),
-    new CompressionPlugin(), 
-  ],
-  devtool: 'source-map',
+        patterns: [
+          { from: "src/images", to: "./" },
+        ],
+      }),
+  ]
 };
