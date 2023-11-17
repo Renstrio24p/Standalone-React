2e3c0fd40932ad6a9b7decebb594f88514114d76
Waren Gador <waren.gador15@gmail.com>
2023-11-17 21:03:21 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -20,17 +20,8 @@ module.exports = (argv) => {
 
   const devServerOptions = {
     port: 4500,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8800',
-        secure: false,
-        changeOrigin: true,
-      },
-    },
-    static: {
-      directory: path.join(__dirname, 'src'),
-      publicPath,
-    },
+    proxy: { '/api': { target: 'http://localhost:8800', secure: false, changeOrigin: true } },
+    static: { directory: path.join(__dirname, 'src'), publicPath },
     open: true,
     hot: !isProduction,
     liveReload: !isProduction,
@@ -48,120 +39,29 @@ module.exports = (argv) => {
     },
     target: 'web',
     devServer: devServerOptions,
-    resolve: {
-      extensions: ['.js', '.jsx', '.json'],
-    },
+    resolve: { extensions: ['.js', '.jsx', '.json'] },
     module: {
       rules: [
-        // JavaScript rule
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'esbuild-loader',
-            options: {
-              loader: 'jsx',
-              target: 'es2015',
-              minify: isProduction,
-            },
-          },
-        },
-        // CSS rule
-        {
-          test: /\.(c|sa|sc)ss$/,
-          exclude: /\.module\.(c|sa|sc)ss$/,
-          use: ['style-loader', 'css-loader', 'sass-loader'],
-        },
-        // CSS modules rule
-        {
-          test: /\.module\.(c|sa|sc)ss$/,
-          use: [
-            'style-loader',
-            {
-              loader: 'css-loader',
-              options: {
-                modules: true,
-              },
-            },
-            'sass-loader',
-          ],
-        },
-        // Image assets rule
-        {
-          test: /\.(png|jpe?g|gif|svg|webp)$/i,
-          type: 'asset/resource',
-          generator: {
-            filename: 'images/[name].[contenthash][ext]',
-          },
-        },
-        // Video assets rule
-        {
-          test: /\.(mp4|webm|ogg|ogv)$/i,
-          type: 'asset/resource',
-          generator: {
-            filename: 'videos/[name].[contenthash][ext]',
-          },
-        },
-        // ... (add more rules as needed)
+        { test: /\.(js|jsx)$/, exclude: /node_modules/, use: { loader: 'esbuild-loader', options: { loader: 'jsx', target: 'es2015', minify: isProduction } } },
+        { test: /\.(c|sa|sc)ss$/, exclude: /\.module\.(c|sa|sc)ss$/, use: ['style-loader', { loader: 'css-loader', options: { importLoaders: 1 } }, 'sass-loader', 'esbuild-loader'] },
+        { test: /\.module\.(c|sa|sc)ss$/, use: ['style-loader', { loader: 'css-loader', options: { modules: true } }, 'sass-loader', 'esbuild-loader'] },
+        { test: /\.(png|jpe?g|gif|svg|webp)$/i, type: 'asset/resource', generator: { filename: 'images/[name].[contenthash][ext]' } },
+        { test: /\.(mp4|webm|ogg|ogv)$/i, type: 'asset/resource', generator: { filename: 'videos/[name].[contenthash][ext]' } },
       ],
     },
     plugins: [
-      new HtmlWebpackPlugin({
-        template: 'index.html',
-      }),
-      new webpack.ProvidePlugin({
-        React: 'react',
-        ReactDOM: 'react-dom',
-      }),
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: 'src/images',
-            to: 'images',
-          },
-          {
-            from: 'src/videos',
-            to: 'videos',
-          },
-        ],
-      }),
+      new HtmlWebpackPlugin({ template: 'index.html' }),
+      new webpack.ProvidePlugin({ React: 'react', ReactDOM: 'react-dom' }),
+      new CopyWebpackPlugin({ patterns: [{ from: 'src/images', to: 'images' }, { from: 'src/videos', to: 'videos' }] }),
       new Dotenv(),
     ],
     optimization: {
       minimize: isProduction,
-      minimizer: [
-        new TerserPlugin({
-          terserOptions: {
-            compress: {
-              drop_console: isProduction,
-            },
-          },
-        }),
-      ],
-      splitChunks: {
-        chunks: 'all',
-        minSize: 0,
-        cacheGroups: {
-          defaultVendors: {
-            test: /[\\/]node_modules[\\/]/,
-            priority: -10,
-          },
-          default: {
-            minChunks: 2,
-            priority: -20,
-            reuseExistingChunk: true,
-          },
-        },
-      },
+      minimizer: [new TerserPlugin({ terserOptions: { compress: { drop_console: isProduction } } })],
+      splitChunks: { chunks: 'all', minSize: 250, cacheGroups: { defaultVendors: { test: /[\\/]node_modules[\\/]/, priority: -10 }, default: { minChunks: 2, priority: -20, reuseExistingChunk: true } } },
     },
-    performance: {
-      hints: isProduction ? 'warning' : false,
-      maxAssetSize: 713 * 1024,
-      maxEntrypointSize: 713 * 1024,
-    },
-    cache: {
-      type: 'filesystem',
-    },
+    performance: { hints: isProduction ? 'warning' : false, maxAssetSize: 450 * 1024, maxEntrypointSize: 450 * 1024 },
+    cache: { type: 'filesystem' },
     stats: 'errors-warnings',
     devtool: isProduction ? 'source-map' : 'eval-source-map',
   };
